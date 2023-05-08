a16c63b96bb51a8222cd07d0338583a8851d382e
andhika pratama putra <120385412+andhikaprttp@users.noreply.github.com>
2023-05-08 10:47:33 +0700

    update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+let data = [62, 35, 45, 47, 56, 58];
+
+// mencari nilai terendah
+let nilaiMin = Math.min(...data);
+
+// mencari nilai tertinggi
+let nilaiMax = Math.max(...data);
+
+// menghitung rentang data
+let rentang = nilaiMax - nilaiMin;
+
+console.log("Rentang data adalah " + rentang);
+
+
+
 function calculateSubnet() {
       const ipAddress = document.getElementById("ip-address").value;
       const subnetMask = document.getElementById("subnet-mask").value;
